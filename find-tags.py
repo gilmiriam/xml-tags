6a4e214430df7d4beda7641350bfc36674e03874
Miriam <gil.miriam.deljulia@gmail.com>
2021-10-04 16:51:09 +0200

    Update find-tags.py

--- find-tags.py
+++ find-tags.py
@@ -5,12 +5,17 @@ import re
 import requests as requests
 from lxml import etree
 
+nice_tree = collections.OrderedDict()
+allTags = collections.OrderedDict()
+xmlTagsAttr = collections.OrderedDict()
+tagsDict = collections.OrderedDict()
+initialTag = 'item'
 url = 'https://www.thaiproperty.com/xmlfeed_hipflat'
+
 r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 with open('data.xml', 'w') as f:
     f.write(r.text)
 element = etree.parse('data.xml')
-nice_tree = collections.OrderedDict()
 
 for tag in element.iter():
     path = re.sub('\[[0-9]+\]', '', element.getpath(tag))
@@ -19,9 +24,6 @@ for tag in element.iter():
     if len(tag.keys()) > 0:
         nice_tree[path].extend(attrib for attrib in tag.keys() if attrib not in nice_tree[path])
 
-allTags = collections.OrderedDict()
-initialTag = 'item'
-
 for path, attribs in nice_tree.items():
     finalPath = path.split(initialTag)
     if len(finalPath) > 1:
@@ -29,38 +31,48 @@ for path, attribs in nice_tree.items():
             allTags[finalPath[1]] = [attribs]
         else:
             allTags[finalPath[1]] = []
-xmlTags = []
-xmlTagsAttr = collections.OrderedDict()
-found = allTags.items()
+
+
+def xml_tree_mapping(count, subCount):
+    if count == 0:
+        return key.replace("/", ">")
+    elif subCount == 0:
+        return key.replace("/", ">")
+
+
 for key, val in allTags.items():
+    key = key.replace('/', '', 1)
     count = 0
     subCount = 0
-    for i, j in found:
+    for i, j in allTags.items():
+        i = i.replace('/', '', 1)
         if key.startswith(i.split('/')[0]):
             if "/" in key:
                 subCount += 1
             else:
                 count += 1
-    if count == 1 and subCount == 0:
-        xmlTags.append(key)
-        xmlTagsAttr[key.replace("/", ">")] = []
-        if len(val) > 0:
-            xmlTagsAttr[key.replace("/", ">")] = val
+    tagger = xml_tree_mapping(count, subCount)
+    if len(val) > 0:
+        xmlTagsAttr[tagger] = val
+    else:
+        xmlTagsAttr[tagger] = []
 
-    if count == 0 and subCount >= 1:
-        xmlTags.append(key.replace("/", ">"))
-        xmlTagsAttr[key.replace("/", ">")] = []
-        if len(val) > 0:
-            xmlTagsAttr[key.replace("/", ">")] = val
+newXmlTags = xmlTagsAttr.copy()
+for tag, attr in newXmlTags.items():
+    found = tag.split('>')
+    if len(found) > 1:
+        if found[0] in xmlTagsAttr:
+            xmlTagsAttr.pop(found[0])
 
 for tag, attr in xmlTagsAttr.items():
-    if len(attr) > 0:
-        results_union = set().union(*attr)
-        for a in results_union:
-            mapper = '{}    string      `xml:"' + initialTag + '{}>{}, attr"`'.format(tag, tag, a)
+    if tag != "":
+        if len(attr) > 0:
+            results_union = set().union(*attr)
+            for a in results_union:
+                mapper = '{}    string      `xml:"{}{}, attr"`'.format(tag.split('>')[-1], tag, a)
+                print(mapper)
+        else:
+            mapper = '{}    string      `xml:"{}"`'.format(tag.split('>')[-1], tag)
             print(mapper)
-    else:
-        mapper = '{}    string      `xml:"' + initialTag + '{}"`'.format(tag, tag)
-        print(mapper)
 
 os.remove('data.xml')
